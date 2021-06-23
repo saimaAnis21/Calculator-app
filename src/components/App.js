4b2e4718488e1c318546044a63e35ffab7638dc3
saimaAnis21 <saima_rehman21@yahoo.com>
2021-06-23 16:14:40 +0500

    Coonect the logic files with display files

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,42 @@
+import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import '../index.css';
 import calculate from '../logic/calculate';
 
-function App() {
-  const answer = 0;
-  const calculator = {
-    total: '0',
-    next: '0',
-    operation: '+/-',
-  };
-  const buttonName = '';
-  return (
-    <>
-      <div className="app">
-        <Display result={answer} />
-        <ButtonPanel />
-        {calculate(calculator, buttonName)}
-      </div>
-    </>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: '0',
+      next: '0',
+      operation: '',
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const dataset = calculate(this.state, buttonName);
+    const { total, next, operation } = dataset;
+    this.setState({
+      total,
+      next,
+      operation,
+    });
+  }
+
+  render() {
+    const { total } = this.state;
+    return (
+      <>
+        <div className="app">
+          <Display result={total} />
+          <ButtonPanel clickHandler={this.handleClick} />
+
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,9 @@ import PropTypes from 'prop-types';
 
 function Button(props) {
   const { name } = props;
+  const handleClick = (buttonName) => props.clickHandler(buttonName);
   return (
-    <button type="submit">
+    <button type="submit" onClick={handleClick.bind(this, name)}>
       {name}
     </button>
   );
@@ -11,6 +12,7 @@ function Button(props) {
 
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  clickHandler: PropTypes.isRequired,
 };
 
 export default Button;
